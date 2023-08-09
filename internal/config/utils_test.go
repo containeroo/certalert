ee652906e8606559e138d87f5ca12b49eb58e117
gi8 <giottolino@gmail.com>
2023-08-09 20:19:20 +0200

    add unittest for ExtractHostAndPort

--- internal/config/utils_test.go
+++ internal/config/utils_test.go
@@ -69,3 +69,38 @@ func TestDeepCopy(t *testing.T) {
 	}
 
 }
+
+func TestExtractHostAndPort(t *testing.T) {
+	tests := []struct {
+		input        string
+		expectedHost string
+		expectedPort int
+		expectedErr  bool
+	}{
+		{"example.com:8080", "example.com", 8080, false},
+		{":1234", "", 1234, false},
+		{"localhost:", "", 0, true},
+		{"localhost:8080", "localhost", 8080, false},
+		{"127.0.0.1:", "", 0, true},
+		{"127.0.0.1:8080", "127.0.0.1", 8080, false},
+		{"invalid", "", 0, true},
+		{"invalid:", "", 0, true},
+	}
+
+	for _, test := range tests {
+		host, port, err := ExtractHostAndPort(test.input)
+
+		if (err != nil) != test.expectedErr {
+			t.Errorf("For %s, expected error: %v, but got: %v", test.input, test.expectedErr, err != nil)
+			continue
+		}
+
+		if host != test.expectedHost {
+			t.Errorf("For %s, expected host: %s, but got: %s", test.input, test.expectedHost, host)
+		}
+
+		if port != test.expectedPort {
+			t.Errorf("For %s, expected port: %d, but got: %d", test.input, test.expectedPort, port)
+		}
+	}
+}
