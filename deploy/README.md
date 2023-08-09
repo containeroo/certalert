c46d31d94e637fc55dcb8a71f931ad6f28b26a24
gi8 <giottolino@gmail.com>
2023-08-09 10:07:17 +0200

    update deploy

--- deploy/README.md
+++ deploy/README.md
@@ -1,5 +1,8 @@
 # Manifests
 
+use `kubectl -k deploy/ -n NAMESPACE` to deploy certalert.
+If you possess certificates with unique requirements that certalert cannot accommodate, you can incorporate an initContainer featuring a customized bash script to extract these certificates. For reference, you can explore the pach-deployment, which serves as an illustrative example.
+
 ## Certificates
 
 To create new certificates you one of the following scripts:
---
+++ deploy/crt-maker.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+SOURCE=$1
+DEST=$2
+
+[ -z ${SOURCE} ] && echo "First argument must be the source directory"
+[ -z ${DEST} ] && echo "Second argument must be the source directory"
+
+[ ! -d ${SOURCE} ] && echo "Source '${SOURCE}' not found!" && exit 1
+[ ! -d ${DEST} ] && echo "Destination '${DEST}' not found!" && exit 1
+
+# remove ending slash
+SOURCE=${SOURCE%/}
+DEST=${DEST%/}
+
+for jks_file in $(find ${SOURCE} -maxdepth 1 -name "*.jks");do
+    base_name=$(basename -s .jks "$jks_file")
+
+    password=$(sed 's/[- \.]/_/g' <<< $base_name)
+    export password="${password^^}_PASSWORD"
+
+    [ -z "${!password}" ] && \
+      echo "'$password' not found in env vars" && \
+      exit 1
+
+    echo "execute: keytool -importkeystore \
+            -srckeystore ${SOURCE}/${base_name}.jks \
+            -srcstorepass ${!password} \
+            -destkeystore /tmp/${base_name}.p12 \
+            -deststorepass ${!password} \
+            -deststoretype PKCS12 "
+
+    keytool -importkeystore \
+            -srckeystore ${SOURCE}/${base_name}.jks \
+            -srcstorepass ${!password} \
+            -destkeystore /tmp/${base_name}.p12 \
+            -deststorepass ${!password} \
+            -deststoretype PKCS12
+
+    echo "execute: openssl pkcs12 -in /tmp/${base_name}.p12 \
+                -nokeys \
+                -out ${DEST}/${base_name}.crt \
+                -passin pass:${!password}"
+
+    openssl pkcs12 -in /tmp/${base_name}.p12 \
+                -nokeys \
+                -out ${DEST}/${base_name}.crt \
+                -passin pass:${!password}
+done
