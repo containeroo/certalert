1b3c89e0b200a0994011782ffccde6aeb03787fa
gi8 <giottolino@gmail.com>
2023-08-17 16:03:48 +0200

    cleanup

--- internal/certificates/utils.go
+++ internal/certificates/utils.go
@@ -44,8 +44,6 @@ func Process(certificates []Certificate, failOnError bool) (certificatesInfo []C
 			certificateInfo, err = ExtractPEMCertificatesInfo(cert.Name, certData, cert.Password, failOnError)
 		case "jks":
 			certificateInfo, err = ExtractJKSCertificatesInfo(cert.Name, certData, cert.Password, failOnError)
-		case "custom":
-			certificateInfo, err = ExtractCustomCertificatesInfo(cert.Name, certData, cert.Password, failOnError)
 		default:
 			// Cannot happen, as the config is validated before
 			// Only here to make the linter happy :)
