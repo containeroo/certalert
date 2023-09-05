1b0199431f4fa927bcbe49afbce18d432e7af209
gi8 <giottolino@gmail.com>
2023-09-05 14:13:46 +0200

    split process into own file

--- internal/certificates/utils.go
+++ internal/certificates/utils.go
@@ -2,7 +2,6 @@ package certificates
 
 import (
 	"fmt"
-	"os"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -15,59 +14,6 @@ func GetCertificateByName(name string, certificates []Certificate) (*Certificate
 		}
 	}
 	return nil, fmt.Errorf("Certificate '%s' not found", name)
-}
-
-// Process extracts certificate information from the certificates and updates the Prometheus metrics
-func Process(certificates []Certificate, failOnError bool) (certificatesInfo []CertificateInfo, err error) {
-	var certInfoList []CertificateInfo
-
-	for _, cert := range certificates {
-		if cert.Enabled != nil && !*cert.Enabled {
-			log.Debugf("Skip certificate '%s' as it is disabled", cert.Name)
-			continue
-		}
-
-		log.Debugf("Processing certificate '%s'", cert.Name)
-
-		certData, err := os.ReadFile(cert.Path)
-		if err != nil {
-			// Accessibility of the file is checked in the config validation, if reached
-			// here, the file exists but can't be read for some reason.
-			if err := handleError(&certInfoList, cert.Name, cert.Type, fmt.Sprintf("Failed to read certificate file '%s'. %v", cert.Path, err), failOnError); err != nil {
-				return nil, err
-			}
-			continue
-		}
-
-		// If user used FileExtensionsToType to specify the type, we need to convert it to the canonical type
-		inferredType, found := FileExtensionsToType[cert.Type]
-		if !found {
-			// This should never happen as the config validation ensures that the type is valid
-			if err := handleError(&certInfoList, cert.Name, cert.Type, fmt.Sprintf("Unknown certificate type '%s'", cert.Type), failOnError); err != nil {
-				return nil, err
-			}
-			continue
-		}
-
-		extractFunc, found := TypeToExtractionFunction[inferredType]
-		if !found {
-			// This should never happen as the config validation ensures that the type is valid
-			if err := handleError(&certInfoList, cert.Name, cert.Type, fmt.Sprintf("Unknown certificate type '%s'", cert.Type), failOnError); err != nil {
-				return nil, err
-			}
-			continue
-		}
-
-		certs, err := extractFunc(cert.Name, certData, cert.Password, failOnError)
-		if err != nil {
-			// err is only returned if failOnError is true
-			return nil, fmt.Errorf("Error extracting certificate information: %v", err)
-		}
-
-		certInfoList = append(certInfoList, certs...)
-	}
-
-	return certInfoList, nil
 }
 
 // certExistsInSlice checks if a certificate exists in a slice
